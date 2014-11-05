3e09b49ed7358b17a53f5ae26b0f4559d2955305
Toni Strandell <toni.strandll@gmail.com>
2014-11-05 22:58:26 +0200

    Add mongo db and store user's moves access token in there

---
+++ mongoConnect.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mongo dogen.mongohq.com:10039/GreatBalls -u ballmover -pjDk3-rP2m9.aCv4-kRE39
--- server/main.js
+++ server/main.js
@@ -1,78 +1,91 @@
 var express = require("express")
 var logfmt = require("logfmt")
 var Moves = require('moves')
-var app = express()
 
-var MOVES_ACCESS_TOKEN_COOKIE = 'mO4cc7Ok3n'
+var app = express()
 
 app.use(logfmt.requestLogger())
 app.use(express.bodyParser())
 app.use(express.static(__dirname + '/../www'))
 
-
 var moves = new Moves({
     api_base: 'https://api.moves-app.com/api/1.1'
   , client_id: 'w26SoC75O014NGR_uNMm666I9b5tR8W5'
   , client_secret: 'OH0IWmwA_2Pfei2XY0Yrd664GFq47fk_vNlP5LVUGZ9_FDa21wW75SvOB756pG1Y'
-  , redirect_uri: 'http://great-balls.herokuapp.com/api/receiveToken'
+  , redirect_uri: 'https://great-balls.herokuapp.com/api/receiveToken'
 })
 
-var dev = {
-    toni_access_token: '8UtD8pCntnG60KlwvbohV17on4z0izUpt9bhbbPA0z58lX2SJ0kRYoYYQBYq21tl'
+var config = {
+  mongo_uri: process.env.MONGODB_URL || 'mongodb://ballmover:jDk3-rP2m9.aCv4-kRE39@dogen.mongohq.com:10039/GreatBalls',
+  mongo_collections: ['users']
 }
+var db = require("mongojs").connect(config.mongo_uri, config.mongo_collections)
 
-app.get('/api/month/current', function (req, res) {
-  var accessToken = getAccessTokenFromCookie(req)
-  if (accessToken) {
-    myMoves.month(accessToken, function (activity) { 
-      res.send(activity)
-    })
-  }
+app.get('/api/cycling/:username/:year/:month', function (req, res) {
+  var username = req.params.username
+  var year = req.params.year
+  var month = req.params.month
+  db.users.findOne({ 'username': username }, function (err, user) {
+    if (err) { return res.status(500).json({ msg: err }) }
+    else if (!user) { return res.status(404).json({ msg: 'no such user: ' + username }) }
+
+    moves.get('/user/summary/daily/' + year + month, user.access_token, movesResponseHandler(function (error, monthJson) {
+      if (error) { return res.status(500).json({ msg: error }) }
+      res.json(monthJson)
+    }))
+  })
 })
 
-app.get('/api/authorize', function (req, res) {
-  myMoves.askAuthorizationFromUser(res)
+app.get('/api/authorize/:username', function (req, res) {
+  var username = req.params.username
+  if (!username) { return res.status(400).json({ msg: 'correct url is "/api/authorize/:username"' })}
+  // TODO: check username does not yet exist in db
+  myMoves.askAuthorizationFromUser(res, username)
 })
 
 app.get('/api/receiveToken', function (req, res) {
   var authToken = req.query.code
+  var username = req.query.state
   if (authToken) {
     myMoves.getAccessToken(authToken, function (error, token) {
-      if (error) { console.error(error); return res.send(error) }
-      console.log('### ' + token.access_token)
-      console.log('### ' + token.refresh_token)
-      console.log('### ' + token.expires_in)
-      res.cookie(MOVES_ACCESS_TOKEN_COOKIE, token.access_token)
-      res.redirect('/')
+      if (error) { console.error(error); return res.status(500).json({ msg: error }) }
+      var newUser = {
+        username: username
+      , access_token: token.access_token
+      , refresh_token: token.refresh_token
+      , expires_in: token.expires_in
+      }
+      console.log(' >> Adding new user: ' + JSON.stringify(newUser))
+
+      db.users.save(newUser, function(err, saved) {
+        if( err || !saved ) { console.error('ERROR saving user: ' + err); res.status(500).json({ msg: err }) }
+        console.log(" >> User saved to mongo\n")
+        // TODO: redirect to page with username , e.g. res.redirect('/' + username)
+        res.redirect('/api/cycling/' + username + '/2014/03')
+      })
     })
   }
-  else { return res.status(400).json({msg: '"code" url parameter missing'}) }
+  else { return res.status(400).json({ msg: '"code" url parameter missing' })}
 })
 
-function getAccessTokenFromCookie(req) {
-  if (req.query.devCookie) return dev.toni_access_token
-  else return req.cookies && req.cookies[MOVES_ACCESS_TOKEN_COOKIE]
-}
-
 var myMoves = {
-  askAuthorizationFromUser: function (res) {
+  askAuthorizationFromUser: function (res, username) {
     moves.authorize({
       scope: ['activity', 'location'] //can contain either activity, location or both
-    , state: 'GreatBalls' //optional state as per oauth
+    , state: username
     }, res)
   }  
 , getAccessToken: function (authCode, callback) {
-    moves.token(authCode, function (error, response, body) {
-      callback(error, JSON.parse(body))
-    })
+    moves.token(authCode, movesResponseHandler(callback))
   }
 , month: function (accessToken, callback) {
     var now = new Date(), year = now.getFullYear(), month = now.getMonth() + 1
-    moves.get('/user/summary/daily/' + year + month, accessToken, function (error, response, body) {
-      if (error) { return console.error(error) }
-      callback(JSON.parse(body))
-    })
+    moves.get('/user/summary/daily/' + year + month, accessToken, movesResponseHandler(callback))
   }
+}
+
+function movesResponseHandler(callback) { 
+  return function (error, response, body) { callback(error, JSON.parse(body)) }
 }
 
 console.log('\n**********************')
